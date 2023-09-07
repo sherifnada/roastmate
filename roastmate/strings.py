2b0b78f08bb4e2f6b832578c784838d8849222c9
Brandon Gell <brgell@gmail.com>
2023-09-07 11:48:40 -0400

    Update strings.py

--- roastmate/strings.py
+++ roastmate/strings.py
@@ -1,32 +1,28 @@
 GROUP_WELCOME_MESSAGE = """
-Hello new frenemies! I’m Roastmate – apparently one of you thinks someone here could use some roasting 😈
+Hello frenemies! 
+
+I’m Roastmate – apparently one of you thinks someone here could use some roasting 😈
+
 Here’s how this works:
-👋 Keep messaging how you normally do, I don’t bite.
-🔥 When someone says something that they deserve to get roasted on I MIGHT chime in.
+🔥 Keep messaging the group how you normally do. When someone says something that they deserve to get roasted on I'll chime in.
 🗑️ I don’t save your messages for more than a couple of days. IDGAF about what you have to say, just sick burns. 
+🗣️ Roasts hit harder when we’re on a first name basis. You can change your name by messaging me “Roastmate please call me YOUR NAME". 
+For example: Roastmate please call me Sam
 
-A few things to really make this work:
-🍤 Add me to your contacts so you can share this number next time you get catfished
-💬 Roasts hit harder when we’re on a first name basis. When I send you a DM lmk your name
-📸 Follow me on Twitter and Insta… when you get roasted good send me a DM of a screenshot and I’ll feature you.
-https://twitter.com/Roastmate_ai
-https://www.instagram.com/roastmate.ai/
-
-Carry on, people!
+🎉 Let's get this party started... what's your name?
 """
 
 
 DM_WELCOME_MESSAGE = """
-Hello new frenemy! I’m Roastmate 👋
+Hello new frenemy! I’m Roastmate 👋. You can find more info about me at www.roastmate.ai
 
 A few things before we get started:
 🗑️ I don’t save any of your message for more than a couple of days. IDGAF about what you have to say, just sick burns. 
-📲 You can message me directly, add me to an existing group, or start a group with me and whoever you want. Your mom, landlord, boss, ex… I don’t care… I’ll roast you all. Try it out, I dare you!
-🍤 Add me to your contacts so you can share this number next time you get catfished
-📸 Follow me on Twitter and Insta… when you get roasted good send me a DM of a screenshot and I’ll feature you.
+📲 You can message me directly, add me to an existing group, or start a group with me and whoever you want. Your mom, landlord, boss, ex… I don’t care… I’ll roast you all.
+🍤 Add me to your contacts so you can share this number next time you get catfished.
+📸 Follow me on Twitter and Instagram. Next time you get roasted good send me a DM of a screenshot and I’ll feature you on my page.
 https://twitter.com/Roastmate_ai
 https://www.instagram.com/roastmate.ai/
 
-Lastly, roasts hit harder when we’re on a first name basis. 
-What's your first name?
+🗣️ Lastly, you can always change your name by messaging me “Roastmate please call me YOUR NAME". For example: Roastmate please call me Sam
 """
