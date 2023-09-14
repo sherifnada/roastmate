fa691651ad2eacdefef34ad98347d356af270d74
Brandon Gell <brgell@gmail.com>
2023-09-14 14:39:18 -0400

    Updated copy to include information about calling to roastmate.py (#5)

--- roastmate/strings.py
+++ roastmate/strings.py
@@ -1,30 +1,42 @@
 GROUP_WELCOME_MESSAGE = """
 Hello frenemies! 
 
-I’m Roastmate – apparently one of you thinks someone here could use some roasting 😈
+I’m Roastmate – apparently one of you thinks someone here could use some roasting.
 
 Here’s how this works:
-🔥 Keep messaging the group how you normally do. When someone says something that they deserve to get roasted on I'll chime in.
-🗑️ I don’t save your messages for more than a couple of days. IDGAF about what you have to say, just sick burns. 
-🗣️ Roasts hit harder when we’re on a first name basis. You can change your name by messaging me “Roastmate please call me YOUR NAME". 
-For example: Roastmate please call me Sam
+🔥 Keep messaging the group how you normally do. I'll only chime in occasionally.
 
-🎉 Let's get this party started... what's your name?
+🗑️ I don’t save your messages for more than a couple of days. IDGAF about what you have to say, just sick burns.
+
+🪄 You can "call to" me at anytime by mentioning "Roastmate" in a message. When you do this I'll always respond. With great power comes great responsibility.
+
+🗣️ Roasts hit harder when we’re on a first name basis. You can change your name by messaging me “Roastmate please call me _______". For example: Roastmate please call me Sam
+
+😈 👹 😈 👹 😈 👹 😈 👹 😈
+
+So... what's your name?
 """
 
 
 DM_WELCOME_MESSAGE = """
-👋 Hello new frenemy! I'm Roastmate. You can find more info about me at www.roastmate.ai
+Hello new frenemy!
+
+I'm Roastmate. You can find more info about me at www.roastmate.ai
 
 Let's get a few things straight:
-
 🍤 Add me to your contacts so you can share this number next time you get catfished.
+
+🗑️ I don’t save your messages for more than a couple of days. IDGAF about what you have to say, just sick burns.
+
 📸 Follow me on Twitter and Instagram. Next time you get roasted good send me a DM of a screenshot and I’ll feature you on my page.
 https://twitter.com/Roastmate_ai
 https://www.instagram.com/roastmate.ai
-https://roastmate.ai
 
-🗣️ Lastly, roasts hit harder when we’re on a first name basis. Respond in the format "Roastmate please call me NAME". For example: Roastmate please call me Rachel
+🪄 You can "call to" me at anytime by mentioning "Roastmate" in a message. When you do this I'll always respond. With great power comes great responsibility.
 
-You can always change it later so try not to think about it too hard...
+🗣️ Roasts hit harder when we’re on a first name basis. You can change your name by messaging me “Roastmate please call me _______". For example: Roastmate please call me Sam
+
+😈 👹 😈 👹 😈 👹 😈 👹 😈
+
+So... what's your name?
 """
