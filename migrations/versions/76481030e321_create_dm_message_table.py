41f90441371e09f12b56c5804f37b6d7b3792f5b
Sherif Nada <snadalive@gmail.com>
2023-09-17 23:22:45 -0400

    fix migration

--- migrations/versions/76481030e321_create_dm_message_table.py
+++ migrations/versions/76481030e321_create_dm_message_table.py
@@ -21,7 +21,8 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     op.create_table(
         "dm_message",
-        sa.Column("from_number", sa.TEXT),
+        sa.Column("sender_number", sa.TEXT),
+        sa.Column("with_number", sa.TEXT),
         sa.Column("date_sent", sa.TIMESTAMP(timezone=True)),
         sa.Column("date_received", sa.TIMESTAMP(timezone=True), index=True),
         sa.Column("content", sa.TEXT),
